6b320f808e205d43017426145815ee74422cb186
403NotFound <1214972923@qq.com>
2022-11-09 22:51:22 +0800

    feat: project init again

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,24 +1,24 @@
-import { Component, PropsWithChildren } from 'react'
+import { Component } from 'react'
 import { View, Text } from '@tarojs/components'
-import './index.styl'
+import './index.scss'
+import { AtButton } from 'taro-ui'
 
-export default class Index extends Component<PropsWithChildren> {
+export default class Index extends Component {
   componentWillMount() {}
 
   componentDidMount() {}
 
   componentWillUnmount() {}
 
-  componentDidShow() {
-    console.log('show')
-  }
+  componentDidShow() {}
 
   componentDidHide() {}
 
   render() {
     return (
       <View className="index">
-        <Text>Hello world12w!</Text>
+        <Text>Hello world!</Text>
+        <AtButton type="primary">按钮文案</AtButton>
       </View>
     )
   }
